331ce3fd9187dd6d39b4bf60738ba719f4c83e9c
fedtoscano <federicotoscano611@gmail.com>
2024-05-17 19:00:41 +0200

    update css and display status of the secret message

--- vital-message/script/script.js
+++ vital-message/script/script.js
@@ -1,39 +1,53 @@
-let secretMessageEl = document.getElementById("secret-message")
-secretMessageEl.innerHTML="";
-let userDifficulty = Number.parseInt(prompt("Select the difficulty (type 4-10)"),10)
-while(isNaN(userDifficulty)|| userDifficulty<4|| userDifficulty>10){
-    userDifficulty = Number.parseInt(prompt("Select the difficulty (type 4-10). Type a valid number!"),10)
-}
-
-const secretWord = []
-function makeRandomStr(strLength){
-    for(i=0; i<strLength; i++){
-        const secretNum = Math.floor(Math.random()*26);
-        const secretCode = String.fromCharCode(65 + secretNum)
-        secretWord.push(secretCode)
-    }    
-    return secretWord.join("")
-}
-
-function relayTheMessage(){
-    const userGuess = prompt("Type your guess...")
-    if(userGuess === secretWord.join("")){
-        console.log("You won! The message has been relayed")
-        secretMessageEl.innerHTML="";
-    }else{
-        console.log("You lost! The message was incorrect")
-        console.log(`You typed: ${userGuess}`);
-        console.log(`The message to relay was: ${secretWord.join("")}`)
-        secretMessageEl.innerHTML="";
+function playAGame(){
+    const messageContainer = document.querySelector(".message-display")
+    let secretMessageEl = document.getElementById("secret-message")
+    secretMessageEl.innerHTML="";
+    messageContainer.style.display = "block"
+    
+    
+    let userDifficulty = Number.parseInt(prompt("Select the difficulty (type 4-10)"),10)
+    while(isNaN(userDifficulty)|| userDifficulty<4|| userDifficulty>10){
+        userDifficulty = Number.parseInt(prompt("Select the difficulty (type 4-10). Type a valid number!"),10)
     }
+
+    
+    function makeRandomStr(strLength){
+        const secretWord = []
+        for(i=0; i<strLength; i++){
+            const secretNum = Math.floor(Math.random()*26);
+            const secretCode = String.fromCharCode(65 + secretNum)
+            secretWord.push(secretCode)
+        }    
+        return secretWord.join("")
+    }
+    
+    function relayTheMessage(message, displayEl){
+        const userGuess = prompt("Type your guess...")
+        if(userGuess.toUpperCase() === message){
+            console.log("You won! The message has been relayed")
+        }else{
+            console.log("You lost! The message was incorrect")
+            console.log(`You typed: ${userGuess}`);
+            console.log(`The message to relay was: ${message}`)
+        }
+        displayEl.innerHTML="";
+        
+    }
+    
+    const secretMessage = makeRandomStr(userDifficulty);
+    secretMessageEl.innerHTML = secretMessage;
+    
+    setTimeout(() => {
+        messageContainer.style.display = "none"
+        secretMessageEl.remove();
+    }, 1500);
+    
+    setTimeout(() => {
+        relayTheMessage(secretMessage, secretMessageEl)
+    }, 3000);
+
+
 }
 
-secretMessageEl.innerHTML = makeRandomStr(userDifficulty);
-
-setTimeout(() => {
-    secretMessageEl.remove();
-}, 1500);
-
-setTimeout(() => {
-    relayTheMessage()
-}, 3000);
+const startBtn = document.querySelector("button#start-a-game")
+startBtn.addEventListener('click', playAGame)
