392439a661fa28d64207d8c175f6757e9d7fca6b
fedtoscano <federicotoscano611@gmail.com>
2024-05-18 00:16:28 +0200

    update variable names

--- shootout/script/main.js
+++ shootout/script/main.js
@@ -1,19 +1,23 @@
-
+/**
+ * 
+ * @returns a random number between 1000 and 1500
+ */
 function makeRandomInterval(){
     return Math.floor(Math.random()* (1500-1000)) + 1000
 }
 
-
 function stepsMaker() {
-    let index = 1;
+    let currentStep = 1;
 
     function step() {
-        console.log(`${index}...`);
-        if (index < 10) {
-            index++;
+        console.log(`${currentStep}...`);
+        if (currentStep < 10) {
+            currentStep++;
             setTimeout(step, makeRandomInterval());
         }
     }
 
     setTimeout(step, makeRandomInterval());
 }
+
+
